85411a58e506503378fb13451ebe845d2f8d284e
Hussam <hussam@dominantstrategies.io>
2024-09-23 11:51:44 -0500

    Unlimited epochs

--- libprogpow/ProgPow.h
+++ libprogpow/ProgPow.h
@@ -4,7 +4,7 @@
 #include <string>
 
 // blocks before changing the random program
-#define PROGPOW_PERIOD          4320
+#define PROGPOW_PERIOD          9999999
 // lanes that work together calculating a hash
 #define PROGPOW_LANES           16
 // uint32 registers per lane
@@ -20,7 +20,7 @@
 // random math instructions per loop
 #define PROGPOW_CNT_MATH        18
 
-#define EPOCH_LENGTH            4320
+#define EPOCH_LENGTH            9999999
 
 class ProgPow
 {
