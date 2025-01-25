b6b294944e9fc828b089768b7cf6e2a873902e84
Hussam <hussam@dominantstrategies.io>
2025-01-25 14:55:23 -0600

    Lower epoch to 50

--- libprogpow/ProgPow.h
+++ libprogpow/ProgPow.h
@@ -20,7 +20,7 @@
 // random math instructions per loop
 #define PROGPOW_CNT_MATH        18
 
-#define EPOCH_LENGTH            129600 // 4320 primes per day * 30 days
+#define EPOCH_LENGTH            50 // 4320 primes per day * 30 days
 
 class ProgPow
 {
